0d0768186b7db0ada754f460fbc0ebfca41b6da2
albi23 <piekielny7891>
2020-03-11 17:57:00 +0100

    correct extension rename

--- ListSecond/changecase.py
+++ ListSecond/changecase.py
@@ -6,8 +6,9 @@ def run_bash_script(path: str, direction: str):
     output_file_format = ",,"
     if direction in "-u":
         output_file_format = "^^"
-    command = 'find ' + path + ' -type f  -exec bash -c \'file_name=${1##*/};file_path=${1%\/*\.*};mv -T -v -i \"$1\" \"${file_path}/${file_name' + output_file_format + '}\"\' -- {} \;'
-    print(command)
+    command = 'find ' + path + ' -type f  -exec bash -c ' \
+                               '\'file_name=${1##*/};file_ext=${file_name##*\.};file_without_extension=${file_name%\.*};file_path=${1%\/*\.*};' \
+                               'mv -T -v -i \"$1\" \"${file_path}/${file_without_extension' + output_file_format + '}.${file_ext}\"\' -- {} \;'
     print(os.popen(command).read())
 
 
